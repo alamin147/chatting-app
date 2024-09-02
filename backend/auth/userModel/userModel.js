ae0e18e0dbfe174f5b5d6abf4319d69601e8a3bb
alamin.147 <alamin.14780@gmail.com>
2024-09-02 19:11:17 +0600

    messages model created

--- backend/auth/userModel/userModel.js
+++ backend/auth/userModel/userModel.js
@@ -1,35 +1,40 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: [true, "Name is required"],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Name is required"],
+    },
+    username: {
+      type: String,
+      require: [true, "Username is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: [true, "Password is required"],
+      minlength: 6,
+    },
+    gender: {
+      type: String,
+      require: [true, "Password is required"],
+      enum: ["male", "female"],
+    },
+    email: {
+      type: String,
+      require: [true, "Email is required"],
+      unique: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
   },
-  username: {
-    type: String,
-    require: [true, "Username is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    require: [true, "Password is required"],
-    minlength: 6,
-  },
-  gender: {
-    type: String,
-    require: [true, "Password is required"],
-    enum: ["male", "female"],
-  },
-  email: {
-    type: String,
-    require: [true, "Email is required"],
-    unique: true,
-  },
-  profilePic: {
-    type: String,
-    default: "",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
